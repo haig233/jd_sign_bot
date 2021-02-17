d05efd3e97a040737abaf99ed1f65c2d24126326
LittleC <i@ltlec.cn>
2021-02-17 15:11:56 +0800

    :zap: improvements

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 // detail url: https://github.com/ruicky/jd_sign_bot
 
 const exec = require('child_process').execSync;
-const fs = require('fs');
-const rp = require('request-promise');
-const download = require('download');
+const axios = require('axios');
+const fs = require('fs')
 
 // 公共变量
 const KEY = process.env.JD_COOKIE;
@@ -16,27 +15,30 @@ const DualKey = process.env.JD_COOKIE_2;
 async function downFile() {
   const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
   // const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
-  await download(url, './');
+  let r = await axios(url)
+  fs.writeFileSync("./JD_DailyBonus.js", r.data)
 }
 
 async function changeFile() {
   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
   if (DualKey) {
+    console.log('has dual key')
     content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
   }
   await fs.writeFileSync('./JD_DailyBonus.js', content, 'utf8')
 }
 
 async function sendNotify(text, desp) {
-  const options = {
-    uri: `https://sc.ftqq.com/${serverJ}.send`,
-    form: {text, desp},
-    json: true,
+  const qs = require('qs')
+  await axios({
+    url: `https://sc.ftqq.com/${serverJ}.send`,
+    data: qs.stringify({
+      text, desp
+    }),
     method: 'POST'
-  }
-  await rp.post(options).then(res => {
-    console.log(res)
+  }).then(res => {
+    console.log(res.data)
   }).catch((err) => {
     console.log(err)
   })
@@ -54,50 +56,43 @@ async function start() {
   await changeFile();
   console.log('替换变量完毕')
   // 执行
-  await exec("node JD_DailyBonus.js >> result.txt");
+  await exec("node JD_DailyBonus.js > result.txt");
   console.log('执行完毕')
 
   if (serverJ) {
+    const { EOL } = require('os');
     const path = "./result.txt";
-    let content = "";
-    if (fs.existsSync(path)) {
-      content = fs.readFileSync(path, "utf8");
+    if (!fs.existsSync(path)) {
+      return
     }
-    let thisTime, jingdou
-    let reward = content.match(/【签到奖励】:.*/g)
-    if (reward) {
-      // 签到奖励匹配成功时,显示获得京豆数
-
-      //移除标题文本/空格,从文本中分割出京豆,将钢镚丢弃
-      thisTime = reward[0].split(":")[1]
-        .replace(" ", "")
-        .split("京豆")[0]
-      if (thisTime !== "获取失败")
-        thisTime = "获得" + thisTime + "京豆"
-      else
-        thisTime = "签到奖励获取失败"
+    const lines = fs.readFileSync(path).toString().split(EOL).filter(v => v)
+    let notify = ""
+    let first = lines.findIndex(v => v.includes("【签到号一】"))
+    let second = lines.findIndex(v => v.includes("【签到号二】"))
+    const totalCount = parseInt(lines[first + 2].match(/【账号总计】:  ([0-9]*)京豆/)[1])
+    if (lines[first + 1].includes("【签到概览】")) {
+      // failed
+      notify += `号一失败 共${totalCount}`
     } else {
-      // 签到奖励匹配失败时,显示成功/失败数
-      let overview = content.match(/【签到概览】:.*/g)[0]
-      //移除标题文本/空格
-      thisTime = overview
-        .replace(/\s+/g,"")
-        .replace(",", "")
-        .split(":")[1]
-      thisTime = "签到" + thisTime
+      // succeed
+      const got = parseInt(lines[first + 1].match(/【签到奖励】:  ([0-9]*)京豆/)[1])
+      notify += `号一成功 得${got}共${totalCount}`
     }
-    // 账号总计
-    let total = content.match(/【账号总计】:.*/g)[0]
-    //移除标题文本
-    jingdou = total
-      .replace(/\s+/g,"")
-      .split(":")[1]
-    if (jingdou !== "获取失败")
-      jingdou = "总计" + jingdou.split("京豆")[0] + "京豆"
-    else
-      jingdou = "京豆数获取失败"
-    await sendNotify(`${thisTime}___${jingdou}`, content);
-    console.log(`${thisTime}___${jingdou}`)
+    if (second && DualKey) {
+      notify += " "
+      const secondTotalCount = parseInt(lines[second + 2].match(/【账号总计】:  ([0-9]*)京豆/)[1])
+      if (lines[second + 1].includes("【签到概览】")) {
+        // failed
+
+        notify += `号二失败 共${secondTotalCount}`
+      } else {
+        // succeed
+        const got = parseInt(lines[second + 1].match(/【签到奖励】:  ([0-9]*)京豆/)[1])
+        notify += `号二成功 得${got}共${secondTotalCount}`
+      }
+    }
+    console.log(notify)
+    await sendNotify(notify.split(' ').join('_'), lines.join(EOL))
   }
 }
 
